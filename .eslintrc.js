b42035e9584437a312e6f4afcfb1f8a3a2b8955d
Swashata Ghosh <swashata@wpquark.com>
2018-03-19 15:03:15 +0530

    chore: add eslint config file
    Initiate and add .eslintrc.js file for linting files.

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "wordpress",
+    "parserOptions": {
+		"ecmaFeatures": {
+            "jsx": true,
+            "experimentalObjectRestSpread": true
+        },
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+		],
+		"space-in-parens": [
+			"error",
+			"always",
+			{
+				"exceptions": ["empty"]
+			}
+		],
+		"space-unary-ops": [
+            'error', {
+                "words": true,
+                "nonwords": false,
+                "overrides": {
+                    "!": true
+                }
+            }
+        ]
+    }
+};
