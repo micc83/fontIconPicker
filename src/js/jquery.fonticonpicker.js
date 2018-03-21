bd3366cc241b7efd09d1904d7b24fb027f73799d
Swashata Ghosh <swashata@wpquark.com>
2018-03-21 09:21:43 +0530

    refactor: fix all eslint errors
    Mostly are due to spacing and some are due to inconsistent usage
    of var. Fix the spacing and replace var with let. We will
    refactor further to use const and arrow functions as applicable.
    
    See #44

--- src/js/jquery.fonticonpicker.js
+++ src/js/jquery.fonticonpicker.js
@@ -16,42 +16,44 @@ import jQuery from 'jquery';
 'use strict';
 
 // Lightweight plugin wrapper
-jQuery.fn.fontIconPicker = function (options) {
+jQuery.fn.fontIconPicker = function( options ) {
 
 	// Instantiate the plugin
-	this.each(function () {
-		if (!jQuery.data(this, "fontIconPicker")) {
-			jQuery.data(this, "fontIconPicker", new FontIconPicker(this, options));
+	this.each( function() {
+		if ( ! jQuery.data( this, 'fontIconPicker' ) ) {
+			jQuery.data( this, 'fontIconPicker', new FontIconPicker( this, options ) );
 		}
-	});
+	} );
 
 	// setIcons method
-	this.setIcons = jQuery.proxy(function (newIcons, iconSearch) {
+	this.setIcons = jQuery.proxy( function( newIcons, iconSearch ) {
 		if ( undefined === newIcons ) {
 			newIcons = false;
 		}
 		if ( undefined === iconSearch ) {
 			iconSearch = false;
 		}
-		this.each(function () {
-			jQuery.data(this, "fontIconPicker").settings.source = newIcons;
-			jQuery.data(this, "fontIconPicker").settings.searchSource = iconSearch;
-			jQuery.data(this, "fontIconPicker").initSourceIndex();
-			jQuery.data(this, "fontIconPicker").resetSearch();
-			jQuery.data(this, "fontIconPicker").loadIcons();
-		});
-	}, this);
+		this.each( function() {
+			jQuery.data( this, 'fontIconPicker' ).settings.source = newIcons;
+			jQuery.data( this, 'fontIconPicker' ).settings.searchSource = iconSearch;
+			jQuery.data( this, 'fontIconPicker' ).initSourceIndex();
+			jQuery.data( this, 'fontIconPicker' ).resetSearch();
+			jQuery.data( this, 'fontIconPicker' ).loadIcons();
+		} );
+	}, this );
 
 	// destroy method
-	this.destroyPicker = jQuery.proxy(function() {
-		this.each(function() {
-			if (!jQuery.data(this, "fontIconPicker")) {
+	this.destroyPicker = jQuery.proxy( function() {
+		this.each( function() {
+			if ( ! jQuery.data( this, 'fontIconPicker' ) ) {
 				return;
 			}
+
 			// Remove the iconPicker
-			jQuery.data(this, "fontIconPicker").iconPicker.remove();
+			jQuery.data( this, 'fontIconPicker' ).iconPicker.remove();
+
 			// Reset the CSS
-			jQuery.data(this, "fontIconPicker").element.css({
+			jQuery.data( this, 'fontIconPicker' ).element.css( {
 				visibility: '',
 				top: '',
 				position: '',
@@ -65,28 +67,29 @@ jQuery.fn.fontIconPicker = function (options) {
 				border: '',
 				verticalAlign: '',
 				float: ''
-			});
+			} );
 
 			// destroy data
-			jQuery.removeData(this, "fontIconPicker");
-		});
-	}, this);
+			jQuery.removeData( this, 'fontIconPicker' );
+		} );
+	}, this );
 
 	// reInit method
-	this.refreshPicker = jQuery.proxy(function(newOptions) {
+	this.refreshPicker = jQuery.proxy( function( newOptions ) {
 		if ( ! newOptions ) {
 			newOptions = options;
 		}
+
 		// First destroy
 		this.destroyPicker();
 
 		// Now reset
-		this.each(function() {
-			if (!jQuery.data(this, "fontIconPicker")) {
-				jQuery.data(this, "fontIconPicker", new Plugin(this, newOptions));
+		this.each( function() {
+			if ( ! jQuery.data( this, 'fontIconPicker' ) ) {
+				jQuery.data( this, 'fontIconPicker', new Plugin( this, newOptions ) );
 			}
-		});
-	}, this);
+		} );
+	}, this );
 
 	return this;
 };
